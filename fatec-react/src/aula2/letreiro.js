584fa340f4898a6dc7df39958937dabf52bcef73
rnbatista92 <rnbatista92@gmail.com>
2023-04-05 22:16:38 -0300

    primeiro

---
+++ fatec-react/src/aula2/letreiro.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
+function Letreiro() {
+  const [texto, setTexto] = useState('');
+  const [indice, setIndice] = useState(0);
+
+  const mensagem = 'Venha estudar na Fatec';
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (indice < mensagem.length) {
+        setTexto(texto + mensagem.charAt(indice));
+        setIndice(indice + 1);
+      } else {
+        setTexto('');
+        setIndice(0);
+      }
+    }, 200);
+
+    return () => clearTimeout(timer);
+  }, [texto, indice]);
+
+  return <h1>{texto}</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Letreiro />
+    </div>
+  );
+}
+
+export default App;
---
+++ fatec-react/src/aula2/relogio.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <h1>Relógio</h1>
+      <p>{time.toLocaleTimeString()}</p>
+    </div>
+  );
+}
+
+export default Clock;
