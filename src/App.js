991a72832df91c639786b4ed4180e58399cc5eae
rnbatista92 <rnbatista92@gmail.com>
2023-05-16 20:21:57 -0300

    atualização

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Letreiro from './aula2/letreiro';
-import Clock from './aula2/relogio';
-import Gallery from './aula4/galeria';
-import { Profile } from './aula4/galeria.js';
-import TodoList from './aula4/todolist';
-import Avatar from './aula4/avatar.js';
-import PackingList from './aula4/packinglist';
-import List from './aula4/list';
+import Letreiro from './aula1/letreiro';
+import Clock from './aula1/relogio';
+import Gallery from './aula3/galeria';
+import { Profile } from './aula3/galeria.js';
+import TodoList from './aula3/todolist';
+import Avatar from './aula3/avatar.js';
+import PackingList from './aula3/packinglist';
+import List from './aula3/list';
 
 function App() {
   return (
     <div>
-      <h1>Aula 2</h1>
+      <h1>Aula 1</h1>
       <Letreiro/>
 
       <Clock/>
       <hr></hr>
-      <h1>Aula 4</h1>
+      <h1>Aula 3</h1>
       <Profile/>
       <TodoList/>
       <Gallery/>
